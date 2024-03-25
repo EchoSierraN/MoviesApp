047bfcd30e28f3502cfa9619262e38be1099d64a
Elias Shemsu <echosierran@gmail.com>
2024-03-25 11:42:56 +0300

    BUGFIX for popular movies

--- app/src/main/java/com/example/moviesapp/movieList/data/remote/MovieApi.kt
+++ app/src/main/java/com/example/moviesapp/movieList/data/remote/MovieApi.kt
@@ -1,5 +1,6 @@
 package com.example.moviesapp.movieList.data.remote
 
+import com.example.moviesapp.movieList.util.Category
 import retrofit2.http.GET
 import retrofit2.http.Path
 import retrofit2.http.Query
@@ -8,11 +9,11 @@ interface MovieApi {
 
     companion object {
         const val BASE_URL = "https://api.themoviedb.org/3/"
-        const val IMAGE_BASE_URL = "https://image.themoviedb.org/t/p/w500"
+        const val IMAGE_BASE_URL = "https://image.tmdb.org/t/p/w500"
         const val API_KEY = "fd3bf26d5e58aeb9e2abdc8680e4d8c9"
     }
 
-    @GET
+    @GET("movie/{category}")
     suspend fun getMoviesList(
         @Path("category") category: String,
         @Query("page") page: Int,
